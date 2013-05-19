e99805964eb2ceb181f82e8308a202eb9511eb2b
xsc10 <xushichang2008@yeah.net>
2013-05-19 11:31:31 +0800

    Change the incident of code.

--- TI/cra/main.c
+++ TI/cra/main.c
@@ -4,149 +4,149 @@ static char firstAlpha(char *s);
 
 int main(int argc, char **argv)
 {
-   buffLen = BUFFLEN;
-   isLinkcra = 0;
+    buffLen = BUFFLEN;
+    isLinkcra = 0;
    
-   strcpy(local6addr,"2001:da8:bf:19::7");
-   strcpy(remote6addr,"2001:da8:bf:19::3");
+    strcpy(local6addr,"2001:da8:bf:19::7");
+    strcpy(remote6addr,"2001:da8:bf:19::3");
    
-   //Read the arguments and operate
-   int index = 2;
-   if (argc < 2) {
-      show_help();
-      return 0;
-   }
+    //Read the arguments and operate
+    int index = 2;
+    if (argc < 2) {
+        show_help();
+        return 0;
+    }
    
-   while (index <= argc) {
-      switch (firstAlpha(argv[index - 1])) {
-         case 'a' ://set local6addr and remote6addr
+    while (index <= argc) {
+        switch (firstAlpha(argv[index - 1])) {
+        case 'a' ://set local6addr and remote6addr
             if (argc < index + 2) {
-               printf("[4over6 CRA]: wrong number of arguments.\n");
-               return 1;
+                printf("[4over6 CRA]: wrong number of arguments.\n");
+                return 1;
             }
             strcpy(local6addr,argv[index]);
             strcpy(remote6addr,argv[index + 1]);
             index += 3;
             break;
-         case 'b' ://set physic device name
+        case 'b' ://set physic device name
             printf("warning : option -b is not used any more!\n");
             index += 2;
             break;
-         case 'c' ://set tunnel device name
+        case 'c' ://set tunnel device name
             printf("warning : option -c is not used any more!\n");
             index += 2;
             break;
-         case 'd' ://run with default configuration
+        case 'd' ://run with default configuration
             if (index == 2) {
-               printf("[4over6 CRA]:Continue with default configuration.\n");
-               index = argc + 1;
+                printf("[4over6 CRA]:Continue with default configuration.\n");
+                index = argc + 1;
             }
             index ++;
             break;
-         case 'l' ://set LCRA device name
+        case 'l' ://set LCRA device name
             printf("[4over6 CRA]:Run in link-cra mode.\n");
             isLinkcra = 1;
             index += 2;
             break;
-         case 'h' ://help info
-         default :
+        case 'h' ://help info
+        default :
             show_help();
-         return 0;
-      } 
-   }
-   //Show current configuration
-   printf("Local ipv6 address : %s\nRemote ipv6 address: %s\n",local6addr,remote6addr);
+            return 0;
+        } 
+    }
+    //Show current configuration
+    printf("Local ipv6 address : %s\nRemote ipv6 address: %s\n",local6addr,remote6addr);
 
-   inet_pton(AF_INET6,remote6addr,remote6addr_buf);	 
-   remote_addr6.sin6_family = AF_INET6;
-   inet_pton(AF_INET6, remote6addr, &(remote_addr6.sin6_addr));
+    inet_pton(AF_INET6,remote6addr,remote6addr_buf);	 
+    remote_addr6.sin6_family = AF_INET6;
+    inet_pton(AF_INET6, remote6addr, &(remote_addr6.sin6_addr));
 
-   initTable();	 
-   initDevice();
-   initSocket();
-   printf("[4over6 CRA]: Listening...\n");
-   getFakeReply();
-   //CRA auto-machine
-   int type;
-   while (1) {
-      getPacket();
-      type= getPacketType();
-      if (isLinkcra ==1 && type == 1)//recv an arp package
-      {
-         recvArpPkt=(struct arppkt *)buff;
-         if(recvArpPkt->arppart.ar_op==htons(1))//recv a request arp package
-         {	
-            //printf("[4over6 CRA]:received an arp request package!\n");
-            if(inSamePhysicalSubnet(recvArpPkt->arppart.ar_sip,recvArpPkt->arppart.ar_tip)!=1)
-            {
-               //printf("[4over6 CRA]:send a proxy arp!");
-               sendProxyArp(recvArpPkt);	 
-            } 
-         }		   
-      }
+    initTable();	 
+    initDevice();
+    initSocket();
+    printf("[4over6 CRA]: Listening...\n");
+    getFakeReply();
+    //CRA auto-machine
+    int type;
+    while (1) {
+        getPacket();
+        type= getPacketType();
+        if (isLinkcra ==1 && type == 1)//recv an arp package
+        {
+            recvArpPkt=(struct arppkt *)buff;
+            if(recvArpPkt->arppart.ar_op==htons(1))//recv a request arp package
+            {	
+                //printf("[4over6 CRA]:received an arp request package!\n");
+                if(inSamePhysicalSubnet(recvArpPkt->arppart.ar_sip,recvArpPkt->arppart.ar_tip)!=1)
+                {
+                    //printf("[4over6 CRA]:send a proxy arp!");
+                    sendProxyArp(recvArpPkt);	 
+                } 
+            }		   
+        }
 		
-      if (type == 4) {
-         //Find out the DHCP packet from 4over6 interface	
-         if (isUDPpacket(iphead,4) && isDHCPpacket(4, udphead)) {
-            //printf("[4over6 CRA]:receive an DHCP4 package!\n");
-            //Send out DHCPv4-over-v6 packet
-            uint8_t message_type = *(udphead + 8);
-            if (message_type == 1) {
-               //printf("[4over6 CRA]:received an DHCP4 request package!\n");
-               if(isLinkcra == 1)
-               {
-                  updateHostMAC(udphead+8);
-                  sendPacket6(ethhead,udphead,udplen);		   
-               }
-               else //hostcra only handle packages send by local interfaces
-               {
-                  if(isLocal(udphead+8+28)!=0)
-                     sendPacket6(ethhead,udphead,udplen);
-               }
+        if (type == 4) {
+            //Find out the DHCP packet from 4over6 interface	
+            if (isUDPpacket(iphead,4) && isDHCPpacket(4, udphead)) {
+                //printf("[4over6 CRA]:receive an DHCP4 package!\n");
+                //Send out DHCPv4-over-v6 packet
+                uint8_t message_type = *(udphead + 8);
+                if (message_type == 1) {
+                    //printf("[4over6 CRA]:received an DHCP4 request package!\n");
+                    if(isLinkcra == 1)
+                    {
+                        updateHostMAC(udphead+8);
+                        sendPacket6(ethhead,udphead,udplen);		   
+                    }
+                    else //hostcra only handle packages send by local interfaces
+                    {
+                        if(isLocal(udphead+8+28)!=0)
+                            sendPacket6(ethhead,udphead,udplen);
+                    }
 									
+                }
             }
-         }
-      } else if (type == 6) {
-         //find out the DHCPv4-over-v6 packet from physic interface
-         if (isUDPpacket(iphead,6) && isDHCPpacket(6, udphead)) {
-            //Send back DHCPv4 packet to 4over6 interface.
-            uint8_t message_type = *(udphead + 8);
-            if (message_type == 2) {
-               //printf("[4over6 CRA]:received an DHCP6 ack package!\n");
-               sendPacket4(ethhead,udphead,udplen);
-               if(isLinkcra && isDHCPAck(udphead+8)) 
-               {
-                  struct if_hostInfo * hostInfo=updateHostIP(udphead+8); 
-                  if(hostInfo != NULL)
-                     addRoute(hostInfo);
-               }				   
+        } else if (type == 6) {
+            //find out the DHCPv4-over-v6 packet from physic interface
+            if (isUDPpacket(iphead,6) && isDHCPpacket(6, udphead)) {
+                //Send back DHCPv4 packet to 4over6 interface.
+                uint8_t message_type = *(udphead + 8);
+                if (message_type == 2) {
+                    //printf("[4over6 CRA]:received an DHCP6 ack package!\n");
+                    sendPacket4(ethhead,udphead,udplen);
+                    if(isLinkcra && isDHCPAck(udphead+8)) 
+                    {
+                        struct if_hostInfo * hostInfo=updateHostIP(udphead+8); 
+                        if(hostInfo != NULL)
+                            addRoute(hostInfo);
+                    }				   
+                }
             }
-         }
-      }
-   }
+        }
+    }
 	  
-   closeSocket();
-   printf("[4over6 CRA]: Closed.\n");
-   return 0;
+    closeSocket();
+    printf("[4over6 CRA]: Closed.\n");
+    return 0;
 }
 
 static char firstAlpha(char *s)
 {
-   while (s && *s && *s == '-')
-      s++;
-   return s ? *s : 0;
+    while (s && *s && *s == '-')
+        s++;
+    return s ? *s : 0;
 }
 //UI Function	 ===========================
 void show_help(void)
 {
-   printf("Usage: cra [-d] | [-a <local_ipv6_addr> <remote_ipv6_addr>]\n");
-   printf("		   [-l LCRA_ITERFACE_NAME]\n");
+    printf("Usage: cra [-d] | [-a <local_ipv6_addr> <remote_ipv6_addr>]\n");
+    printf("		   [-l LCRA_ITERFACE_NAME]\n");
 //	 printf("-h : display this help information.\n");
-   printf("-d : run program with default settings.\n");
-   printf("-a : set local and remote ipv6 address.\n");
-   printf("-l : set the LCRA mode.\n");//the name of the local interface which runs LCRA.\n");
+    printf("-d : run program with default settings.\n");
+    printf("-a : set local and remote ipv6 address.\n");
+    printf("-l : set the LCRA mode.\n");//the name of the local interface which runs LCRA.\n");
 //	 printf("-b : set physic device (interface) name.\n");
 //	 printf("-c : set the name of the interface on which runs the DHCP client.\n");
-   return ;
+    return ;
 }
 
--- TI/cra/packet.c
+++ TI/cra/packet.c
@@ -2,283 +2,283 @@
 
 unsigned char* getDhcpOptions(unsigned char *dhcpHead)
 {
-   return dhcpHead+240;
+    return dhcpHead+240;
 }
 
 
 unsigned char *getDhcpOption(unsigned char *dhcpHead, unsigned char type)
 {
-   unsigned char *dhcpOption;
-   dhcpOption=getDhcpOptions(dhcpHead);
-   while(*dhcpOption!=0xFF)
-   {
-      if(*dhcpOption==type)
-      {
-         return dhcpOption;  
-      }
-      dhcpOption++;
-      dhcpOption+=(*dhcpOption+1);
-   }
-   return NULL;
+    unsigned char *dhcpOption;
+    dhcpOption=getDhcpOptions(dhcpHead);
+    while(*dhcpOption!=0xFF)
+    {
+        if(*dhcpOption==type)
+        {
+            return dhcpOption;  
+        }
+        dhcpOption++;
+        dhcpOption+=(*dhcpOption+1);
+    }
+    return NULL;
 }
 
 int isUDPpacket(char* iphead, int type)
 {
-   switch (type)
-   {
-      case 4:
-         if (iphead[9] != IPPROTO_UDP)
-         {
+    switch (type)
+    {
+    case 4:
+        if (iphead[9] != IPPROTO_UDP)
+        {
             //printf("[4over6 CRA]: Got a v4 packet but is not UDPv4.\n");
             return 0;
-         }
-         break;
-      case 6:
-         if (iphead[6] != IPPROTO_UDP)
-         {
+        }
+        break;
+    case 6:
+        if (iphead[6] != IPPROTO_UDP)
+        {
             //printf("[4over6 CRA]: Got a v6 packet but is not UDPv6.\n");
             return 0;
-         }
-         break;
-      default:
-         //printf("[4over6 CRA]: Wrong argument of isUDPpacket.\n");
-         return 0;
-         break;
-   }
-   return 1;    
+        }
+        break;
+    default:
+        //printf("[4over6 CRA]: Wrong argument of isUDPpacket.\n");
+        return 0;
+        break;
+    }
+    return 1;    
 }
 
 int isDHCPpacket(int type, char* udphead)
 {
-   uint16_t src_port = ntohs(*(uint16_t*)(udphead + 0));
-   uint16_t dst_port = ntohs(*(uint16_t*)(udphead + 2));
+    uint16_t src_port = ntohs(*(uint16_t*)(udphead + 0));
+    uint16_t dst_port = ntohs(*(uint16_t*)(udphead + 2));
 //printf("src_port = %d, dst_port = %d\n",src_port, dst_port);
-   if (type == 4) {
-      if (dst_port != 67 /*|| src_port != 68*/)
-      {
-         //printf("[4over6 CRA]: Got a packet but not targeted to port %d.\n",dst);
-         return 0;
-      }  
-   }
-   if (type == 6) {
-      if (dst_port != 67 || src_port != 67)
-      {
-         //printf("[4over6 CRA]: Got a packet but not targeted to port %d.\n",dst);
-         return 0;
-      }  
-   }
+    if (type == 4) {
+        if (dst_port != 67 /*|| src_port != 68*/)
+        {
+            //printf("[4over6 CRA]: Got a packet but not targeted to port %d.\n",dst);
+            return 0;
+        }  
+    }
+    if (type == 6) {
+        if (dst_port != 67 || src_port != 67)
+        {
+            //printf("[4over6 CRA]: Got a packet but not targeted to port %d.\n",dst);
+            return 0;
+        }  
+    }
 
-   return 1;
+    return 1;
 }
 
 int isDHCPAck(unsigned char *dhcpHead)
 {
-   unsigned char *dhcptype=getDhcpOption(dhcpHead,0x35);
-   if(dhcptype==NULL)return 0;
+    unsigned char *dhcptype=getDhcpOption(dhcpHead,0x35);
+    if(dhcptype==NULL)return 0;
   
-   if(*(dhcptype+2)==0x05)return 1;
-   return 0;   
+    if(*(dhcptype+2)==0x05)return 1;
+    return 0;   
 }
 
 unsigned short int
 checksum (unsigned short int *addr, int len)
 {
-   int nleft = len;
-   int sum = 0;
-   unsigned short int *w = addr;
-   unsigned short int answer = 0;
+    int nleft = len;
+    int sum = 0;
+    unsigned short int *w = addr;
+    unsigned short int answer = 0;
 
-   while (nleft > 1) {
-      sum += *w++;
-      nleft -= sizeof (unsigned short int);
-   }
+    while (nleft > 1) {
+        sum += *w++;
+        nleft -= sizeof (unsigned short int);
+    }
 
-   if (nleft == 1) {
-      *(unsigned char *) (&answer) = *(unsigned char *) w;
-      sum += answer;
-   }
+    if (nleft == 1) {
+        *(unsigned char *) (&answer) = *(unsigned char *) w;
+        sum += answer;
+    }
 
-   sum = (sum >> 16) + (sum & 0xFFFF);
-   sum += (sum >> 16);
-   answer = ~sum;
-   return (answer);
+    sum = (sum >> 16) + (sum & 0xFFFF);
+    sum += (sum >> 16);
+    answer = ~sum;
+    return (answer);
 }
 
 uint16_t udpchecksum(char *iphead, char *udphead, int udplen, int type)
 {
-   udphead[6] = udphead[7] = 0;
-   uint32_t checksum = 0;
-   //printf("udp checksum is 0x%02x%02x\n", (uint8_t)udphead[6], (uint8_t)udphead[7]);
-   if (type == 6)
-   {
-      struct udp6_psedoheader header;
-      memcpy(header.srcaddr, iphead + 24, 16);
-      memcpy(header.dstaddr, iphead + 8, 16);
-      header.length = ntohs(udplen);
-      header.zero1 = header.zero2 = 0;
-      header.next_header = 0x11;
-      uint16_t *hptr = (uint16_t*)&header;
-      int hlen = sizeof(header);
-      while (hlen > 0) {
-         checksum += *(hptr++);
-         hlen -= 2;
-      }
-   }
-   else if (type == 4)
-   {
-      struct udp4_psedoheader header;
-      memcpy((char*)&header.srcaddr, iphead + 12, 4);
-      memcpy((char*)&header.dstaddr, iphead + 16, 4);
-      header.zero = 0;
-      header.protocol = 0x11;
-      header.length = ntohs(udplen);
-      uint16_t *hptr = (uint16_t*)&header;
-      int hlen = sizeof(header);
-      while (hlen > 0) {
-         checksum += *(hptr++);
-         hlen -= 2;
-      }
-   }    
-   uint16_t *uptr = (uint16_t*)udphead;
-   while (udplen > 1) {    
-      checksum += *(uptr++);
-      udplen -= 2;
-   }
-   if (udplen) {
-      checksum += (*((uint8_t*)uptr)) ;
-   }
-   do {
-      checksum = (checksum >> 16) + (checksum & 0xFFFF);
-   } while (checksum != (checksum & 0xFFFF));
-   uint16_t ans = checksum;
-   return (ans == 0xFF)? 0xFF :ntohs(~ans);
+    udphead[6] = udphead[7] = 0;
+    uint32_t checksum = 0;
+    //printf("udp checksum is 0x%02x%02x\n", (uint8_t)udphead[6], (uint8_t)udphead[7]);
+    if (type == 6)
+    {
+        struct udp6_psedoheader header;
+        memcpy(header.srcaddr, iphead + 24, 16);
+        memcpy(header.dstaddr, iphead + 8, 16);
+        header.length = ntohs(udplen);
+        header.zero1 = header.zero2 = 0;
+        header.next_header = 0x11;
+        uint16_t *hptr = (uint16_t*)&header;
+        int hlen = sizeof(header);
+        while (hlen > 0) {
+            checksum += *(hptr++);
+            hlen -= 2;
+        }
+    }
+    else if (type == 4)
+    {
+        struct udp4_psedoheader header;
+        memcpy((char*)&header.srcaddr, iphead + 12, 4);
+        memcpy((char*)&header.dstaddr, iphead + 16, 4);
+        header.zero = 0;
+        header.protocol = 0x11;
+        header.length = ntohs(udplen);
+        uint16_t *hptr = (uint16_t*)&header;
+        int hlen = sizeof(header);
+        while (hlen > 0) {
+            checksum += *(hptr++);
+            hlen -= 2;
+        }
+    }    
+    uint16_t *uptr = (uint16_t*)udphead;
+    while (udplen > 1) {    
+        checksum += *(uptr++);
+        udplen -= 2;
+    }
+    if (udplen) {
+        checksum += (*((uint8_t*)uptr)) ;
+    }
+    do {
+        checksum = (checksum >> 16) + (checksum & 0xFFFF);
+    } while (checksum != (checksum & 0xFFFF));
+    uint16_t ans = checksum;
+    return (ans == 0xFF)? 0xFF :ntohs(~ans);
 }
 
 struct arppkt *createArpResponse(struct arppkt *recvArpPkt)
 {
-   //construct the arp package to be sent
-   int index=recvDev.sll_ifindex;
-   struct arppkt *sendArpPkt=(struct arppkt*)malloc(sizeof(struct arppkt));
-   memcpy(sendArpPkt->etherpart.ether_dhost,recvArpPkt->etherpart.ether_shost,ETH_ALEN);
-   memcpy(sendArpPkt->etherpart.ether_shost,index2ifaddr(index),ETH_ALEN);
-   memcpy(&sendArpPkt->etherpart.ether_type,&recvArpPkt->etherpart.ether_type,2);
-   sendArpPkt->arppart.ar_hrd=recvArpPkt->arppart.ar_hrd;
-   sendArpPkt->arppart.ar_pro=recvArpPkt->arppart.ar_pro;
-   sendArpPkt->arppart.ar_hln=recvArpPkt->arppart.ar_hln;
-   sendArpPkt->arppart.ar_pln=recvArpPkt->arppart.ar_pln;
-   sendArpPkt->arppart.ar_op=htons(2);
-   memcpy(sendArpPkt->arppart.ar_sha,sendArpPkt->etherpart.ether_shost,ETH_ALEN);
-   memcpy(sendArpPkt->arppart.ar_sip,recvArpPkt->arppart.ar_tip,4);
-   memcpy(sendArpPkt->arppart.ar_tha,recvArpPkt->arppart.ar_sha,ETH_ALEN);
-   memcpy(sendArpPkt->arppart.ar_tip,recvArpPkt->arppart.ar_sip,4);
-   return sendArpPkt;
+    //construct the arp package to be sent
+    int index=recvDev.sll_ifindex;
+    struct arppkt *sendArpPkt=(struct arppkt*)malloc(sizeof(struct arppkt));
+    memcpy(sendArpPkt->etherpart.ether_dhost,recvArpPkt->etherpart.ether_shost,ETH_ALEN);
+    memcpy(sendArpPkt->etherpart.ether_shost,index2ifaddr(index),ETH_ALEN);
+    memcpy(&sendArpPkt->etherpart.ether_type,&recvArpPkt->etherpart.ether_type,2);
+    sendArpPkt->arppart.ar_hrd=recvArpPkt->arppart.ar_hrd;
+    sendArpPkt->arppart.ar_pro=recvArpPkt->arppart.ar_pro;
+    sendArpPkt->arppart.ar_hln=recvArpPkt->arppart.ar_hln;
+    sendArpPkt->arppart.ar_pln=recvArpPkt->arppart.ar_pln;
+    sendArpPkt->arppart.ar_op=htons(2);
+    memcpy(sendArpPkt->arppart.ar_sha,sendArpPkt->etherpart.ether_shost,ETH_ALEN);
+    memcpy(sendArpPkt->arppart.ar_sip,recvArpPkt->arppart.ar_tip,4);
+    memcpy(sendArpPkt->arppart.ar_tha,recvArpPkt->arppart.ar_sha,ETH_ALEN);
+    memcpy(sendArpPkt->arppart.ar_tip,recvArpPkt->arppart.ar_sip,4);
+    return sendArpPkt;
 }
 
 char *trans6to4(char *ethhead, char *udphead, int udplen, int *frame_len)
 {
-   *(uint16_t*)(udphead + 2) = htons(68);
-   char *frame = NULL;
-   udplen += 40;
-   *frame_len = 14 + 20 + udplen;
+    *(uint16_t*)(udphead + 2) = htons(68);
+    char *frame = NULL;
+    udplen += 40;
+    *frame_len = 14 + 20 + udplen;
 	
 	 
-   frame = malloc (sizeof(char) * (*frame_len));
-   //Add ethernet header
-   memcpy(frame, ethhead, 14);
-   memcpy(frame, udphead + 36, 6);
-   frame[12] = ETH_P_IP / 256;
-   frame[13] = ETH_P_IP % 256;
-   //Add ipv4 header
-   send_ip4hdr.ip_hl = 5;
-   send_ip4hdr.ip_v = 4;
-   send_ip4hdr.ip_tos = 0;
-   send_ip4hdr.ip_len = htons(20 + udplen);
-   send_ip4hdr.ip_id = htons(0);
-   send_ip4hdr.ip_off = htons(0);
-   send_ip4hdr.ip_ttl = 255;
-   send_ip4hdr.ip_p = IPPROTO_UDP;
-   //ciaddr is from yiaddr field
-   memcpy(ciaddr,udphead + 24,4);
-   inet_pton(AF_INET,"0.0.0.0",&siaddr);
-   memcpy((char*)&send_ip4hdr.ip_dst,ciaddr,4);
-   memcpy((char*)&send_ip4hdr.ip_src,siaddr,4);
-   send_ip4hdr.ip_sum = 0;
-   send_ip4hdr.ip_sum = checksum((unsigned short int*)&send_ip4hdr,20);
-   memcpy(frame + 14,(char *)&send_ip4hdr, 20);
+    frame = malloc (sizeof(char) * (*frame_len));
+    //Add ethernet header
+    memcpy(frame, ethhead, 14);
+    memcpy(frame, udphead + 36, 6);
+    frame[12] = ETH_P_IP / 256;
+    frame[13] = ETH_P_IP % 256;
+    //Add ipv4 header
+    send_ip4hdr.ip_hl = 5;
+    send_ip4hdr.ip_v = 4;
+    send_ip4hdr.ip_tos = 0;
+    send_ip4hdr.ip_len = htons(20 + udplen);
+    send_ip4hdr.ip_id = htons(0);
+    send_ip4hdr.ip_off = htons(0);
+    send_ip4hdr.ip_ttl = 255;
+    send_ip4hdr.ip_p = IPPROTO_UDP;
+    //ciaddr is from yiaddr field
+    memcpy(ciaddr,udphead + 24,4);
+    inet_pton(AF_INET,"0.0.0.0",&siaddr);
+    memcpy((char*)&send_ip4hdr.ip_dst,ciaddr,4);
+    memcpy((char*)&send_ip4hdr.ip_src,siaddr,4);
+    send_ip4hdr.ip_sum = 0;
+    send_ip4hdr.ip_sum = checksum((unsigned short int*)&send_ip4hdr,20);
+    memcpy(frame + 14,(char *)&send_ip4hdr, 20);
 
-   //Re-caculate the udp checksum
-   uint16_t newchecksum = udpchecksum((char*)&send_ip4hdr, udphead, udplen,4);
-   udphead[6] = (newchecksum >> 8) & 0xFF;
-   udphead[7] = newchecksum & 0xFF;
+    //Re-caculate the udp checksum
+    uint16_t newchecksum = udpchecksum((char*)&send_ip4hdr, udphead, udplen,4);
+    udphead[6] = (newchecksum >> 8) & 0xFF;
+    udphead[7] = newchecksum & 0xFF;
 	 
-   //Add udp header + dhcp data
-   memcpy(frame + 14 + 20, udphead, udplen);
-   memcpy(frame, udphead + 36, 6);
-   return frame;
+    //Add udp header + dhcp data
+    memcpy(frame + 14 + 20, udphead, udplen);
+    memcpy(frame, udphead + 36, 6);
+    return frame;
 }
 
 
 char *trans4to6(char *ethhead, char *udphead, int udplen, int *frame_len)
 {
-   char *frame = NULL;
-   *frame_len = 40 + udplen;
-   frame = malloc (sizeof(char) * (*frame_len));
+    char *frame = NULL;
+    *frame_len = 40 + udplen;
+    frame = malloc (sizeof(char) * (*frame_len));
 
-   *(uint16_t*)udphead = htons(67);
+    *(uint16_t*)udphead = htons(67);
 
-   //Add ipv6 header
-   send_ip6hdr.ip6_flow = htonl((6 << 28) | (0 << 20) | 0);        
-   send_ip6hdr.ip6_plen = htons(udplen);
-   send_ip6hdr.ip6_nxt = IPPROTO_UDP;
-   send_ip6hdr.ip6_hops = 128;
-   inet_pton(AF_INET6,local6addr,&send_ip6hdr.ip6_src);
-   inet_pton(AF_INET6,remote6addr,&send_ip6hdr.ip6_dst);    
-   memcpy(frame, (char *)&send_ip6hdr, 40);
+    //Add ipv6 header
+    send_ip6hdr.ip6_flow = htonl((6 << 28) | (0 << 20) | 0);        
+    send_ip6hdr.ip6_plen = htons(udplen);
+    send_ip6hdr.ip6_nxt = IPPROTO_UDP;
+    send_ip6hdr.ip6_hops = 128;
+    inet_pton(AF_INET6,local6addr,&send_ip6hdr.ip6_src);
+    inet_pton(AF_INET6,remote6addr,&send_ip6hdr.ip6_dst);    
+    memcpy(frame, (char *)&send_ip6hdr, 40);
 
-   //memcpy(udphead + 36, macaddr_phy, 6);
+    //memcpy(udphead + 36, macaddr_phy, 6);
 	 
-   //Re-caculate the udp checksum
-   uint16_t newchecksum = udpchecksum((char*)&send_ip6hdr, udphead, udplen,6);
-   udphead[6] = (newchecksum >> 8) & 0xFF;
-   udphead[7] = newchecksum & 0xFF;
-   //Add udp header + dhcp data
-   memcpy(frame + 40, udphead, udplen);
-   return frame;
+    //Re-caculate the udp checksum
+    uint16_t newchecksum = udpchecksum((char*)&send_ip6hdr, udphead, udplen,6);
+    udphead[6] = (newchecksum >> 8) & 0xFF;
+    udphead[7] = newchecksum & 0xFF;
+    //Add udp header + dhcp data
+    memcpy(frame + 40, udphead, udplen);
+    return frame;
 }
 
 int getPacketType()
 {
-   int type = 0;
-   //Locate the headers of the packet
-   ethhead = buff;
-   if (ethhead[0] == 0x45)// && ethhead[1] == 0x00) {
-   {
-      iphead = ethhead;
-      type = 4;
-      printf("Warning! Recv ipv4 packages without ethernet part.\n");
-   }
-   if(ethhead[12]==0x08&&ethhead[13]==0x06){
-      //printf("arp\n");
-      return 1;
-   }
-   if(ethhead[12]==0x08&&ethhead[13]==0x00){
-      //printf("ipv4\n");
-      iphead = ethhead + 14;
-      udphead = iphead + 20;
-      udplen = ((iphead[2]<<8)&0XFF00 | iphead[3]&0XFF) - 20;
-      return 4;
-   }
-   if(ethhead[12]==0x86&&ethhead[13]==0xdd)
-   {
-      // printf("ipv6\n");
-      iphead = ethhead + 14;
-      udphead = iphead + 40;
-      udplen = ((iphead[4]<<8)&0XFF00 | iphead[5]&0XFF) - 40;
-      return 6;
-   }
-   //printf("head: %x %x\n",ethhead[12],ethhead[13]);
+    int type = 0;
+    //Locate the headers of the packet
+    ethhead = buff;
+    if (ethhead[0] == 0x45)// && ethhead[1] == 0x00) {
+    {
+        iphead = ethhead;
+        type = 4;
+        printf("Warning! Recv ipv4 packages without ethernet part.\n");
+    }
+    if(ethhead[12]==0x08&&ethhead[13]==0x06){
+        //printf("arp\n");
+        return 1;
+    }
+    if(ethhead[12]==0x08&&ethhead[13]==0x00){
+        //printf("ipv4\n");
+        iphead = ethhead + 14;
+        udphead = iphead + 20;
+        udplen = ((iphead[2]<<8)&0XFF00 | iphead[3]&0XFF) - 20;
+        return 4;
+    }
+    if(ethhead[12]==0x86&&ethhead[13]==0xdd)
+    {
+        // printf("ipv6\n");
+        iphead = ethhead + 14;
+        udphead = iphead + 40;
+        udplen = ((iphead[4]<<8)&0XFF00 | iphead[5]&0XFF) - 40;
+        return 6;
+    }
+    //printf("head: %x %x\n",ethhead[12],ethhead[13]);
 	
 
 	 
-   return type;
+    return type;
 }
